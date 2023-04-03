81e9d10e17db5fdcb993eb8876c1a96eab0c82a9
KarolWes <karol.wesolowski01@gmail.com>
2023-04-03 18:33:38 +0200

    math base for similarity

--- assignment3.py
+++ assignment3.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from utilityModule import common_interest
 
 def review_list(user_id:int):
@@ -21,6 +22,35 @@ def cross_reference(user_id:int, common_size:int):
     return cross
 
 
+def norm(vec, p: int = 2):
+    func = lambda x: pow(abs(x), p)
+    res = sum(func(vec))
+    return pow(res, (1/p))
+
+def dot(X, Y):
+    if len(X) != len(Y):
+        raise NameError("Vectors of different length")
+    tmp =[]
+    for i in range(len(X)):
+        tmp.append(X[i]*Y[i])
+    return sum(tmp)
+
+
+def cos_simil(X, Y):
+    ans = 0
+    try:
+        d = dot(X, Y)
+        nX = norm(X)
+        nY = norm(Y)
+        ans =  d/(nX*nY)
+    except NameError as nerr:
+        print(nerr)
+    except ZeroDivisionError:
+        print("Vectors are perpendicular")
+    finally:
+        return ans
+
+
 
 if __name__ == "__main__":
     # print("Give the id of a user you want to predict for: ", end="")
@@ -28,4 +58,7 @@ if __name__ == "__main__":
     # print(f"Some of the movies user {user_id} has rated:")
     # print(review_list(user_id)[["title", "genres"]].head(n=15))
 
-    print(cross_reference(1, 10))
+    # print(cross_reference(1, 10))
+    X = np.array([1,1,1,1])
+    Y = np.array([5,5,5,5])
+    print(cos_simil(X, Y))
