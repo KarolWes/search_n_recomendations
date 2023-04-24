15a2051a4bcb88becfbe6472f6207ad99c2de1b0
Jusuf <jusuftopic@yahoo.com>
2023-04-24 18:48:12 +0200

    Extend with 3D

--- assignment4.py
+++ assignment4.py
@@ -12,10 +12,7 @@ def private_interest_genres(reviews: pd.DataFrame):
 
 
 def predict_simple(predict_size: int, reviews, genres_values: pd.DataFrame, mov_filename: str):
-    reviewed = reviews["movieId_y"].T.values.tolist()
-    all_movies = pd.read_csv(mov_filename)[["id", "original_title", "genres"]]
-    to_predict = all_movies[~all_movies.id.isin(reviewed)]
-    to_predict["genres"] = to_predict["genres"].apply(cleanup_genres)
+    to_predict = get_to_predict_movies(mov_filename, reviews)
     overlap = []
     user_genre_list = genres_values.index
     for _, line in to_predict.iterrows():
@@ -37,6 +34,31 @@ def predict_simple(predict_size: int, reviews, genres_values: pd.DataFrame, mov_
     return to_predict.head(predict_size)
 
 
+def predict_with_genre_count(predict_size: int, reviews, genres_values: pd.DataFrame, mov_filename: str):
+    to_predict = get_to_predict_movies(mov_filename, reviews)
+    genre_counts = []
+
+    for _, line in to_predict.iterrows():
+        count = 0
+        for genre in line["genres"]:
+            if genre in genres_values.index:
+                count += genres_values.loc[genre][0]
+        genre_counts.append(count)
+
+    to_predict["genre_count"] = pd.Series(genre_counts)
+    to_predict = to_predict.sort_values(by=["genre_count"], ascending=False)
+
+    return to_predict.head(predict_size)
+
+
+def get_to_predict_movies(mov_filename, reviews):
+    reviewed = reviews["movieId_y"].T.values.tolist()
+    all_movies = pd.read_csv(mov_filename)[["id", "original_title", "genres"]]
+    to_predict = all_movies[~all_movies.id.isin(reviewed)]
+    to_predict["genres"] = to_predict["genres"].apply(cleanup_genres)
+    return to_predict
+
+
 def get_reviews(user_id: int, rev_filename: str, mov_filename: str):
     try:
         return review_list(user_id, rev_filename, mov_filename)
@@ -49,6 +71,7 @@ if __name__ == "__main__":
     print("Give the id of a user you want to predict for: ", end="")
     user_id = int(input())
     rev = get_reviews(user_id, "data/ratings_small.csv", "data/movies.csv")
-    #print(rev)
+    print(rev)
     genres_value_table = private_interest_genres(rev)
     print(predict_simple(10, rev, genres_value_table, "data/movies.csv"))
+    print(predict_with_genre_count(10, rev, genres_value_table, "data/movies.csv"))
