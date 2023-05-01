2d13f4dc6c2dff590084a6e2babfba4d472c51c6
KarolWes <karol.wesolowski01@gmail.com>
2023-05-01 18:46:00 +0200

    part 2

--- assignment5.py
+++ assignment5.py
@@ -3,7 +3,8 @@ import pandas as pd
 import constants as cs
 from utilityModule import *
 
-def add_titles(reviews:pd.DataFrame, movie_filename):
+
+def add_titles(reviews: pd.DataFrame, movie_filename):
     try:
         movies = movies_cleanup(movie_filename)
     except FileNotFoundError:
@@ -14,6 +15,7 @@ def add_titles(reviews:pd.DataFrame, movie_filename):
     review_movie = review_movie[["userId", "movieId_x", "title", "rating"]]
     review_movie.columns = ["userId", "movieId", "title", "rating"]
     return review_movie
+
 
 def get_user_ratings(dataframe):
     user_ratings_dict = {}
@@ -75,14 +77,17 @@ def make_predictions(test_df, us_ratings):
     return predictions
 
 
-def predictions_readable(predictions, test_df):
+def cleanup_predictions(predictions, test_df, relevance=3):
+    result = []
     i = 0
     for _, row in test_df.iterrows():
         movie_title = row["title"]
         p = predictions[i]
-        if p[1] > 0:
-            print(f"user {p[0]} -> {movie_title}: {p[1]}")
-        i+=1
+        r = 1 if p[1] > relevance else 0
+        result.append((p[0], movie_title, p[1], r))
+        i += 1
+    result = pd.DataFrame(result, columns=["userId", "title", "rating", "relevance"])
+    return result
 
 
 def compute_mea(test_df, predictions):
@@ -92,7 +97,8 @@ def compute_mea(test_df, predictions):
 
 def compute_rmse(test_df, predictions):
     actual_ratings = list(test_df["rating"])
-    return np.sqrt(sum([(actual - pred[1]) ** 2 for actual, pred in zip(actual_ratings, predictions)]) / len(actual_ratings))
+    return np.sqrt(
+        sum([(actual - pred[1]) ** 2 for actual, pred in zip(actual_ratings, predictions)]) / len(actual_ratings))
 
 
 def task1(ratio: float):
@@ -101,30 +107,79 @@ def task1(ratio: float):
     test_data = add_titles(test_data, cs.MOVIE_FILE)
     user_ratings = get_user_ratings(train_data)
     ratings_predictions = make_predictions(test_data, user_ratings)
-    predictions_readable(ratings_predictions, test_data)
+    clean = cleanup_predictions(ratings_predictions, test_data)
+    clean = clean[clean["rating"] > 0]
+    print(clean)
     mea = compute_mea(test_data, ratings_predictions)
     rmse = compute_rmse(test_data, ratings_predictions)
-    print(mea)
-    print(rmse)
+    print(f"mean absolute error: {mea}")
+    print(f"root mean square error: {rmse}")
 
-def precission():
+
+def precision(data):
     # true positive / all
-    pass
+    upper = 0
+    lower = len(data)
+    for _, row in data.iterrows():
+        if row["relevance_x"] == row["relevance_y"] == 1:
+            upper += 1
+    return upper / lower
 
-def recall():
+
+def recall(data):
     # true positive / false negative
-    pass
+    # x -> is; y -> should be
+    upper = 0
+    lower = 0
+    for _, row in data.iterrows():
+        if row["relevance_x"] == row["relevance_y"] == 1:
+            upper += 1
+        if row["relevance_y"] == 1:
+            lower += 1
+    return upper / lower
+
+
+def create_rankings(predictions, lim=10):
+    predictions = predictions.sort_values(["userId", "rating"], ascending=[True, False])
+    counter = 0
+    user = 0
+    ranking = []
+    for i, row in predictions.iterrows():
+        if user != row["userId"]:
+            user = row["userId"]
+            counter = 1
+            ranking.append(row)
+            # that is still wrong
+        else:
+            if counter < lim:
+                ranking.append(row)
+                counter += 1
+            else:
+                continue
+    ranking = pd.DataFrame(ranking, columns=predictions.columns)
+    print(ranking)
+
 
 def task2():
+    print("task 2")
     train_data, test_data = split_dataset(cs.RATINGS_SMALL_FILE, 0.2)
+    train_data = add_titles(train_data, cs.MOVIE_FILE)
+    test_data = add_titles(test_data, cs.MOVIE_FILE)
     user_ratings = get_user_ratings(train_data)
     ratings_predictions = make_predictions(test_data, user_ratings)
+    clean = cleanup_predictions(ratings_predictions, test_data)
+    print(len(clean))
+    print(create_rankings(clean))
+    test_data["relevance"] = test_data["rating"].apply(lambda x: 1 if x > 3 else 0)
+    relevance_data = clean.merge(test_data, how='left', on=['userId', 'title'])
+    print(f"Precision: {precision(relevance_data)}")
+    print(f"Recall: {recall(relevance_data)}")
 
 
 if __name__ == "__main__":
+    pd.set_option('display.max_columns', None)
     print("Enter training-set ratio between 0 and 1: ")
     ratio = float(input())
     task1(ratio)
-
-
-
+    # ___________
+    task2()
